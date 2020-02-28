a581b267d4404714cab6d444d39d8e416650d074
Deadida322 <61591520+Deadida322@users.noreply.github.com>
2020-02-28 18:51:32 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title> Deadida</title>
+</head>
+<body>
+h1, h2, h3 {
+  font-weight: 400;
+  margin: 0;
+}
+.head1 {
+	font-size: 32px;
+	margin-left:10px;
+}
+<p class ="head1">Обо мне</p>
+	
+	
+	
+	   
+</body>
+</html>
